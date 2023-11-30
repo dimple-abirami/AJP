1a4873f34aeaee057473d93e8c1a34ab1e3674ff
dimple-abirami <89069947+dimple-abirami@users.noreply.github.com>
2023-11-30 14:20:59 +0530

    Create networkInterface.java

---
+++ problems solved/networkInterface.java
@@ -0,0 +1,5 @@
+/*
+A Company wants to develop an application to scan the network interface details and a  physical address of a host. Your host needs to collect 
+the host name and then examine the IP  address of host. After that it scans and prints all the possible network interface names found  in the 
+host. And then print the physical address (MAC) of the host. 
+*/
