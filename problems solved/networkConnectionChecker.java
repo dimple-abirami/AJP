f3b3e67f7bea4c0bf92977fa832922920e3bb4ee
dimple-abirami <89069947+dimple-abirami@users.noreply.github.com>
2023-12-01 12:23:13 +0530

    Update networkConnectionChecker.java

--- problems solved/networkConnectionChecker.java
+++ problems solved/networkConnectionChecker.java
@@ -8,36 +8,34 @@ a message “The host is unable to reach”.
 import java.net.*;
 import java.util.*;
 
-public class networkConnectionChecker 
-{
-    public static void main(String[] args){
+public class NetworkConnectionChecker {
+    public static void main(String[] args) {
         String host;
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter Host name : ");
         host = sc.nextLine();
-        
-        try{
+
+        try {
             InetAddress address = InetAddress.getByName(host);
-            if(address.isReachable(128)){
-                System.out.println("HostName : "+address.getHostName());
-                System.out.println("IP Address : "+address.getHostAddress());
+            if (address.isReachable(128)) {
+                System.out.println("HostName : " + address.getHostName());
+                System.out.println("IP Address : " + address.getHostAddress());
                 byte[] add = address.getAddress();
-                System.out.println('IPv%d',(add.length==4)?4:6);
+                System.out.println(String.format("IPv%d", (add.length == 4) ? 4 : 6));
                 int fbyte = add[0];
                 char cls = 'a';
-                if((fbyte & 0x80)==0) cls = 'A';
-                else if((fbyte & 0xC0)==0x80) cls = 'B';
-                else if((fbyte & 0xE0)==0xC0) cls = 'C';
-                else if((fbyte & 0xF0)==0xE0) cls = 'D';
-                
-                System.out.println("Class "+cls);
-            }
-            else{
+                if ((fbyte & 0x80) == 0) cls = 'A';
+                else if ((fbyte & 0xC0) == 0x80) cls = 'B';
+                else if ((fbyte & 0xE0) == 0xC0) cls = 'C';
+                else if ((fbyte & 0xF0) == 0xE0) cls = 'D';
+
+                System.out.println("Class " + cls);
+            } else {
                 System.out.println("The Host is unable to reach");
             }
-        }
-        catch(unknownHostException e){
-            System.out.println("Couldnot find host IP address");
+        } catch (UnknownHostException e) {
+            System.out.println("Could not find host IP address");
         }
     }
 }
+
